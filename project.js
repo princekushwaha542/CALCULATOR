ef06bf6c09bb4b94d25a542517ee48fd051b22b3
Prince Kushwaha <kushwahaprince43668@gmail.com>
2025-12-06 22:14:14 +0530

    js-part

---
+++ project.js
@@ -0,0 +1,28 @@
+// function keys
+let equal=document.querySelector(".equal");
+let plus=document.querySelector(".plus");
+let mainus=document.querySelector(".mainus");
+let into=document.querySelector(".into");
+let slace= document.querySelector(".slace");
+let allC=document.querySelector(".all-clear");
+let deleet=document.querySelector(".deleet");
+let percentage =document.querySelector(".percentage");
+let dot=document.querySelector(".dot");
+
+//input
+let inpBox=document.querySelector("#input");
+
+//keys
+let one=document.querySelector(".one");
+let two=document.querySelector(".two");
+let three=document.querySelector(".three");
+let fore =document.querySelector(".fore");
+let five=document.querySelector(".five");
+let six=document.querySelector(".six");
+let seven=document.querySelector(".seven");
+let eight=document.querySelector(".eight");
+let nine=document.querySelector(".nine");
+let jero=document.querySelector("jero");
+let dbjero=document.querySelector("2-jero");
+
+console.log();
