61b69e166671349b848e3c6192eaa8f6241126d7
Prince Kushwaha <kushwahaprince43668@gmail.com>
2025-12-07 09:44:29 +0530

    all done

--- project.js
+++ project.js
@@ -1,28 +1,55 @@
-// function keys
-let equal=document.querySelector(".equal");
-let plus=document.querySelector(".plus");
-let mainus=document.querySelector(".mainus");
-let into=document.querySelector(".into");
-let slace= document.querySelector(".slace");
-let allC=document.querySelector(".all-clear");
-let deleet=document.querySelector(".deleet");
-let percentage =document.querySelector(".percentage");
-let dot=document.querySelector(".dot");
+// input 
+let input = document.querySelector("#input");
+let btn = document.querySelectorAll("button");
 
-//input
-let inpBox=document.querySelector("#input");
+let string = "";
+let arr = Array.from(btn);
 
-//keys
-let one=document.querySelector(".one");
-let two=document.querySelector(".two");
-let three=document.querySelector(".three");
-let fore =document.querySelector(".fore");
-let five=document.querySelector(".five");
-let six=document.querySelector(".six");
-let seven=document.querySelector(".seven");
-let eight=document.querySelector(".eight");
-let nine=document.querySelector(".nine");
-let jero=document.querySelector("jero");
-let dbjero=document.querySelector("2-jero");
 
-console.log();
+arr.forEach(button =>{
+    button.addEventListener("click",(e) =>{
+        handleInput(e.target.innerHTML);
+    });
+});
+
+
+
+document.addEventListener("keydown", function(e){
+
+    // allowed keys
+    let key = e.key;
+
+    if("0123456789+-*/.%".includes(key)){
+        handleInput(key);
+    }
+    else if(key === "Enter"){
+        handleInput("=");
+    }
+    else if(key === "Backspace"){
+        handleInput("DE");
+    }
+    else if(key === "Escape"){
+        handleInput("AC");
+    }
+});
+
+
+
+function handleInput(value){
+    if(value == '='){
+        string = eval(string);
+        input.value = string;
+    }
+    else if(value == 'AC'){
+        string = "";
+        input.value = string;
+    }
+    else if(value == 'DE'){
+        string = string.slice(0, -1);
+        input.value = string;
+    }
+    else{
+        string += value;
+        input.value = string;
+    }
+}
